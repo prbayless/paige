d35c73803b4c24060ed445d1c9e3e98fe9d7ebdf
Paige Bayless <prbayless@gmail.com>
2013-05-11 12:52:22 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paige::Application.config.secret_token = '5b72624ee298ae56ab75003479725d61d572d52dcaf53f458ad41c050c5957397231bf0a495ea472cbd2ddb670533b1ffc75dd3c353e58ba9050e9603a3ae077'
